895606d4582090dda8764cfca68c44a2c4e2de6b
AJ Meyghani <meyghania@gmail.com>
2020-04-02 23:58:47 -0700

    organize animation objects.

--- intro.js
+++ intro.js
@@ -1,25 +1,4 @@
-const letterAnimationIntro = {
-  targets: ".letter",
-  opacity: {
-    value: [0, 1],
-    easing: "easeInOutSine",
-    duration: 500,
-  },
-  duration: 500,
-  translateY: [-50, 0],
-  delay: anime.stagger(100),
-  easing: "steps(10)",
-};
-
-const letterAnimationOutro = {
-  targets: ".letter",
-  opacity: [1, 0],
-  duration: 2000,
-  translateY: "-=50",
-  delay: anime.stagger(100),
-};
-
-const squareAnimation = {
+const squareMoveRight = {
   targets: ".square",
   translateX: [-50, 50],
   loop: true,
@@ -34,12 +13,34 @@ const scaleUpSquare = {
   duration: 500,
 };
 
-const introAnimation = anime.timeline({
+const lettersIntro = {
+  targets: ".letter",
+  opacity: {
+    value: [0, 1],
+    easing: "easeInOutSine",
+    duration: 500,
+  },
+  duration: 500,
+  translateY: [-50, 0],
+  delay: window.anime.stagger(100),
+  easing: "steps(10)",
+};
+
+const lettersOutro = {
+  targets: ".letter",
+  opacity: [1, 0],
+  duration: 2000,
+  translateY: "-=50",
+  delay: window.anime.stagger(100),
+};
+
+const introAnimation = window.anime.timeline({
   loop: false,
+  delay: 200,
 });
 
 introAnimation
-  .add(squareAnimation)
+  .add(squareMoveRight)
   .add(scaleUpSquare)
-  .add(letterAnimationIntro)
-  .add(letterAnimationOutro, "+=1000");
+  .add(lettersIntro)
+  .add(lettersOutro, "+=1000");
